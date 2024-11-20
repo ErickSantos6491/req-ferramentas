8832ae61c036401cb88bcebc1156dea3671f5035
ErickSantos6491 <erickmirandapt@gmail.com>
2024-11-20 08:52:49 +0000

    Initial commit

---
+++ ReqFerramentas/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ReqFerramentas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnColaboradores = new System.Windows.Forms.Button();
+            this.btnFerramentas = new System.Windows.Forms.Button();
+            this.btnRequisicoes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(305, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Menu";
+            // 
+            // btnColaboradores
+            // 
+            this.btnColaboradores.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColaboradores.Location = new System.Drawing.Point(48, 211);
+            this.btnColaboradores.Name = "btnColaboradores";
+            this.btnColaboradores.Size = new System.Drawing.Size(150, 66);
+            this.btnColaboradores.TabIndex = 1;
+            this.btnColaboradores.Text = "Gerir Colaboradores";
+            this.btnColaboradores.UseVisualStyleBackColor = true;
+            // 
+            // btnFerramentas
+            // 
+            this.btnFerramentas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFerramentas.Location = new System.Drawing.Point(509, 211);
+            this.btnFerramentas.Name = "btnFerramentas";
+            this.btnFerramentas.Size = new System.Drawing.Size(150, 66);
+            this.btnFerramentas.TabIndex = 2;
+            this.btnFerramentas.Text = "Gerir Ferramentas";
+            this.btnFerramentas.UseVisualStyleBackColor = true;
+            // 
+            // btnRequisicoes
+            // 
+            this.btnRequisicoes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRequisicoes.Location = new System.Drawing.Point(284, 211);
+            this.btnRequisicoes.Name = "btnRequisicoes";
+            this.btnRequisicoes.Size = new System.Drawing.Size(150, 66);
+            this.btnRequisicoes.TabIndex = 3;
+            this.btnRequisicoes.Text = "Gerir Requisições";
+            this.btnRequisicoes.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRequisicoes);
+            this.Controls.Add(this.btnFerramentas);
+            this.Controls.Add(this.btnColaboradores);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnColaboradores;
+        private System.Windows.Forms.Button btnFerramentas;
+        private System.Windows.Forms.Button btnRequisicoes;
+    }
+}
+
