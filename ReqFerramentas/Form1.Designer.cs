5721019e1c84f6931d1e62b190b4e8c04e48bba4
ErickSantos6491 <erickmirandapt@gmail.com>
2024-11-20 08:57:04 +0000

    CRUD a Colaboradores

--- ReqFerramentas/Form1.Designer.cs
+++ ReqFerramentas/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(305, 85);
+            this.label1.Location = new System.Drawing.Point(340, 86);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(103, 39);
             this.label1.TabIndex = 0;
@@ -47,17 +47,18 @@
             // btnColaboradores
             // 
             this.btnColaboradores.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnColaboradores.Location = new System.Drawing.Point(48, 211);
+            this.btnColaboradores.Location = new System.Drawing.Point(83, 212);
             this.btnColaboradores.Name = "btnColaboradores";
             this.btnColaboradores.Size = new System.Drawing.Size(150, 66);
             this.btnColaboradores.TabIndex = 1;
             this.btnColaboradores.Text = "Gerir Colaboradores";
             this.btnColaboradores.UseVisualStyleBackColor = true;
+            this.btnColaboradores.Click += new System.EventHandler(this.btnColaboradores_Click);
             // 
             // btnFerramentas
             // 
             this.btnFerramentas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFerramentas.Location = new System.Drawing.Point(509, 211);
+            this.btnFerramentas.Location = new System.Drawing.Point(544, 212);
             this.btnFerramentas.Name = "btnFerramentas";
             this.btnFerramentas.Size = new System.Drawing.Size(150, 66);
             this.btnFerramentas.TabIndex = 2;
@@ -67,7 +68,7 @@
             // btnRequisicoes
             // 
             this.btnRequisicoes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRequisicoes.Location = new System.Drawing.Point(284, 211);
+            this.btnRequisicoes.Location = new System.Drawing.Point(319, 212);
             this.btnRequisicoes.Name = "btnRequisicoes";
             this.btnRequisicoes.Size = new System.Drawing.Size(150, 66);
             this.btnRequisicoes.TabIndex = 3;
